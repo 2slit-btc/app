2042e2e3b6674d55676b0a94579a29b15f27b71f
Ray Victor <ray7551@gmail.com>
2024-05-23 04:36:50 +0800

    update data type

--- app/page.tsx
+++ app/page.tsx
@@ -4,135 +4,48 @@ import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { GithubIcon } from "@/components/icons";
-import L2List, { L2Type } from '@/components/L2List';
+import L2List, { L2 } from '@/components/L2List';
 
-type listResponse = {
-	nextCursor: number;
-	list: {
-		id: number;
-		name: string;
-		type: L2Type;
-		TVL: number;
-		shares: number;
-		icon: string;
-	}[]
+type ChainData = L2 & {
+	creator: string;
+	ctime: string;
+	explorer_url: string;
+	is_deleted: boolean;
+	modifier: string;
+	mtime: string;
 }
-async function getData(): Promise<listResponse> {
-	console.log(process.env.API_BASE_URL + '/list')
+type ChainDataResponse = ChainData[]
 
-	return {
-    "nextCursor": 36,
-    "list": [
-        {
-            "id": 97,
-            "name": "dubw",
-            "type": "Side Chain",
-            "TVL": 6,
-            "shares": 65.60810920890417,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 89,
-            "name": "fvy",
-            "type": "Side Chain",
-            "TVL": 61,
-            "shares": 75.61624939997992,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 53,
-            "name": "ttsq",
-            "type": "Side Chain",
-            "TVL": 34,
-            "shares": 15.052013467421709,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 74,
-            "name": "cuk",
-            "type": "Other",
-            "TVL": 99,
-            "shares": 79.0141118268454,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 7,
-            "name": "lvy",
-            "type": "Other",
-            "TVL": 43,
-            "shares": 45.56727618134434,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 86,
-            "name": "cxff",
-            "type": "Side Chain",
-            "TVL": 40,
-            "shares": 87.12181570394169,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 97,
-            "name": "tmtr",
-            "type": "Other",
-            "TVL": 42,
-            "shares": 77.08523797284693,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 45,
-            "name": "yuir",
-            "type": "ZK-Rollup",
-            "TVL": 16,
-            "shares": 54.502965942087,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 21,
-            "name": "gwgz",
-            "type": "ZK-Rollup",
-            "TVL": 57,
-            "shares": 56.778833547794676,
-            "icon": "http://dummyimage.com/50x50"
-        },
-        {
-            "id": 7,
-            "name": "dnvm",
-            "type": "State Channel",
-            "TVL": 20,
-            "shares": 15.78439166033339,
-            "icon": "http://dummyimage.com/50x50"
-        }
-    ]
+async function getData(): Promise<ChainDataResponse> {
+	console.log(process.env.API_BASE_URL + '/chain_data')
+
+	const res = await fetch(process.env.API_BASE_URL + '/chain_data')
+
+	if (!res.ok) {
+		// TODO use history data
+		console.error('Failed to fetch data', res.body)
+
+		throw new Error('Failed to fetch data')
 	}
 
-  const res = await fetch(process.env.API_BASE_URL + '/list')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
-	console.log(res.json())
- 
-  return res.json()
+	return await res.json()
 }
 
 export default async function Home() {
-  const l2s = await getData()
+	const l2s = await getData()
+	console.log(l2s)
 
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block w-full text-center justify-center">
-				<L2List 
+				<L2List
 					columns={[
-						{ name: "Name", field: "name" },
-						{ name: "Type", field: "type" },
-						{ name: "TVL", field: "TVL" },
-						{ name: "Shares", field: "shares" },
+						{ name: "Name", field: "name", sortable: true, align: "start", valueAlign: 'left' },
+						{ name: "Stage", field: "stage", sortable: false, align: "start", valueAlign: 'center' },
+						{ name: "Price(USD)", field: "price_usd", sortable: true, align: "center", valueAlign: 'right' },
+						{ name: "TVL(USD)", field: "tvl_price_usd", sortable: true, align: "end", valueAlign: 'right' },
 					]}
-					l2s={l2s.list}
+					l2s={l2s}
 				/>
 			</div>
 		</section>
--- components/L2List.tsx
+++ components/L2List.tsx
@@ -1,37 +1,96 @@
 'use client'
 
 import React from "react";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, ChipProps, getKeyValue } from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, ChipProps, SortDescriptor } from "@nextui-org/react";
 
 const statusColorMap: Record<string, ChipProps["color"]> = {
   active: "success",
   paused: "danger",
-  vacation: "warning",
+  test_stage: "warning",
 };
 
 export type L2Type = "Side Chain" | "ZK-Rollup" | "State Channel" | "Other"
 
 export interface L2 {
-  icon: string;
+  /**
+   * 链上手续费(原生代币计价)
+   */
+  fee_token: number;
+  /**
+   * 链上手续费(USD计价)
+   */
+  fee_usd: number;
+  github_url: string;
+  icon_url: string;
+  /**
+   * ID
+   */
   id: number;
+  labels: string;
+  /**
+   * 市值(USD计价)
+   */
+  market_value: number;
   name: string;
+  native_token_name: string;
   /**
-   * market share, including 2 decimals
+   * 原生代币价格(USD计价)
    */
-  shares: number;
+  price_usd: number;
+  stage: string;
   /**
-   * in Million USD
+   * TVL中的BTC量
    */
-  TVL: number;
-  type: L2Type;
+  tvl_btc_amount: number;
+  /**
+   * TVL中的BTC总价(USD计价)
+   */
+  tvl_btc_price_usd: number;
+  /**
+   * TVL中的ETH量
+   */
+  tvl_eth_amount: number;
+  /**
+   * TVL中的ETH总价(BTC计价)
+   */
+  tvl_eth_price_btc: number;
+  /**
+   * TVL中的ETH总价(USD计价)
+   */
+  tvl_eth_price_usd: number;
+  /**
+   * TVL中的非BTC&ETH代币的总价(BTC计价)
+   */
+  tvl_other_price_btc: number;
+  /**
+   * TVL中的非BTC&ETH代币的总价(USD计价)
+   */
+  tvl_other_price_usd: number;
+  /**
+   * TVL总量(BTC计价)
+   */
+  tvl_price_btc: number;
+  /**
+   * TVL总量(USD计价)
+   */
+  tvl_price_usd: number;
+  twitter_url: string;
 }
 
 export type L2Columns = {
   name: string;
   field: keyof L2;
+  sortable?: boolean;
+  align?: "start" | "center" | "end";
+  valueAlign?: "left" | "center" | "right";
 }[]
 
 export default function L2List({ l2s, columns }: { l2s: L2[], columns: L2Columns }) {
+  const [sortDescriptor, setSortDescriptor] = React.useState<SortDescriptor>({
+    column: "tvl_price_usd",
+    direction: "descending",
+  })
+
   const renderCell = React.useCallback((l2: L2, columnKey: React.Key) => {
     const cellValue = l2[columnKey as keyof L2];
 
@@ -39,28 +98,28 @@ export default function L2List({ l2s, columns }: { l2s: L2[], columns: L2Columns
       case "name":
         return (
           <User
-            avatarProps={{ radius: "lg", src: l2.icon }}
+            avatarProps={{ radius: "lg", src: l2.icon_url }}
             name={cellValue}
           >
             {l2.name}
           </User>
         );
-      case "type":
+      case "stage":
         return (
-          <Chip className="capitalize" color={statusColorMap[l2.type]} size="sm" variant="flat">
+          <Chip className="capitalize border-none gap-1 text-default-600" color={statusColorMap[l2.stage]} size="sm" variant="dot">
             {cellValue}
           </Chip>
         );
-      case "TVL":
+      case "tvl_price_usd":
         return (
-          <div className="relative flex items-end gap-2">
-            {cellValue} M
+          <div className="relative flex gap-2">
+            ${cellValue}
           </div>
         );
-      case "shares":
+      case "price_usd":
         return (
-          <div className="relative flex items-end gap-2">
-            {Number(cellValue).toFixed(2)} %
+          <div className="relative flex gap-2">
+            ${Number(cellValue).toFixed(2)}
           </div>
         );
       default:
@@ -68,19 +127,55 @@ export default function L2List({ l2s, columns }: { l2s: L2[], columns: L2Columns
     }
   }, []);
 
+  const classNames = React.useMemo(
+    () => ({
+      wrapper: ["max-h-[382px]", "max-w-3xl"],
+      th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
+      td: [
+        // changing the rows border radius
+        // first
+        "group-data-[first=true]:first:before:rounded-none",
+        "group-data-[first=true]:last:before:rounded-none",
+        // middle
+        "group-data-[middle=true]:before:rounded-none",
+        // last
+        "group-data-[last=true]:first:before:rounded-none",
+        "group-data-[last=true]:last:before:rounded-none",
+      ],
+    }),
+    [],
+  )
+
+  const sortedItems = React.useMemo(() => {
+    return l2s.sort((a, b) => {
+      const first = a[sortDescriptor.column as keyof L2] as number;
+      const second = b[sortDescriptor.column as keyof L2] as number;
+      const cmp = first < second ? -1 : first > second ? 1 : 0;
+
+      return sortDescriptor.direction === "descending" ? -cmp : cmp;
+    });
+  }, [sortDescriptor, l2s])
+
   return (
-    <Table aria-label="2slit.btc table" removeWrapper>
+    <Table aria-label="2slit.btc table" classNames={classNames}
+      onSortChange={setSortDescriptor}
+      removeWrapper>
       <TableHeader columns={columns}>
         {(column) => (
-          <TableColumn key={column.field} align={column.field === "type" ? "center" : "start"}>
+          <TableColumn key={column.field}
+            align={column.align}
+            allowsSorting={column.sortable}
+          >
             {column.name}
           </TableColumn>
         )}
       </TableHeader>
-      <TableBody items={l2s}>
+      <TableBody items={sortedItems}>
         {(item) => (
           <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+            {(columnKey) => <TableCell>{
+              renderCell(item, columnKey)
+            }</TableCell>}
           </TableRow>
         )}
       </TableBody>
